b83dcbabc40deaca20c2d9a84e1d46723a0f50c6
Mahrshi <mahrshibaxi@gmail.com>
2020-03-04 18:26:36 -0500

    update

---
+++ lab_4/scripts/main.js
@@ -0,0 +1,28 @@
+
+
+let myImage = document.querySelector('img')
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src')
+    if(mySrc == 'images/firefox-icon.png'){
+        myImage.setAttribute('src', 'images/firefox2.png')  
+    } else{
+        myImage.setAttribute('src','images/firefox-icon.png');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function myUserName(){
+    let myName= prompt('Please enter your name');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool' + myName;
+    
+}
+
+if(!localStorage.getItem('name')){
+    setUsername();
+} else{
+    let storedName = localStorage.setItem('name',myName);
+    myHeading.innerHTML = 'Mozilla is cool' + myName;
+
+}
