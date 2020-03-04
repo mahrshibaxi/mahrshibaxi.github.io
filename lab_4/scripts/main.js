7b04e0c80c0614dff5d6e856c156fcb1890511ed
Mahrshi <mahrshibaxi@gmail.com>
2020-03-04 18:33:49 -0500

    Update

--- lab_4/scripts/main.js
+++ lab_4/scripts/main.js
@@ -4,9 +4,9 @@ let myImage = document.querySelector('img')
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src')
     if(mySrc == 'images/firefox-icon.png'){
-        myImage.setAttribute('src', 'images/firefox2.png')  
+        myImage.setAttribute('src', 'images')  
     } else{
-        myImage.setAttribute('src','images/firefox-icon.png');
+        myImage.setAttribute('src','images/');
     }
 }
 
