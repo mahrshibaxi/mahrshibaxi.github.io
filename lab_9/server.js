37d9375be70e1a6e72e63582ff32cd555bf9897f
mahrshibaxi <44123829+mahrshibaxi@users.noreply.github.com>
2020-04-19 18:14:21 -0400

    Update

--- lab_9/server.js
+++ lab_9/server.js
@@ -22,13 +22,45 @@ app.use(express.static('public'));
 
 
 function processDataForFrontEnd(req, res) {
-  const baseURL = ''; // Enter the URL for the data you would like to retrieve here
+  const baseURL = "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json"; // Enter the URL for the data you would like to retrieve here
 
   // Your Fetch API call starts here
   // Note that at no point do you "return" anything from this function -
   // it instead handles returning data to your front end at line 34.
     fetch(baseURL)
       .then((results) => results.json())
+      .then((data) => {
+          const clearEmptyData = data.filter((f) => f.geocoded_column_1);
+          const refined = clearEmptyData.map((m) => ({
+            category: m.category,
+            name: m.name,
+            latLong: m.geocoded_column_1.coordinates,
+          }));
+          return refined;
+        })
+
+        .then((data) => {
+          return data.reduce((result, current) => {
+            if (!result[current.category]) {
+              result[current.category] = [];
+            }
+            result[current.category].push(current);
+            return result;
+          });
+        })
+
+        .then((data) => {
+          console.log("new data", data);
+          const reformattedData = Object.entries(data).map((m, i) => {
+            console.log(m);
+            return {
+              y: m[1].length,
+              label: m[0],
+            };
+          });
+          return reformattedData
+        })
+
       .then((data) => {
         console.log(data);
         res.send({ data: data }); // here's where we return data to the front end
@@ -45,3 +77,4 @@ function processDataForFrontEnd(req, res) {
 app.get('/api', (req, res) => {processDataForFrontEnd(req, res)});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
