c8b5a64073fc77cc6ad806df3d9725cb9b398a1a
mahrshibaxi <44123829+mahrshibaxi@users.noreply.github.com>
2020-04-24 18:40:33 -0400

    Update

--- lab_10/libraries/writeuser.js
+++ lab_10/libraries/writeuser.js
@@ -6,7 +6,9 @@ async function writeUser(username, dbSettings) {
     const db = await open(dbSettings)
     await db.exec('CREATE TABLE IF NOT EXISTS user (name)');
     await db.exec(`INSERT INTO user VALUES ("${username}")`);
-    const result = await db.each('SELECT * FROM user');
+    const result = await db.each('SELECT * FROM user',(err)=>{
+      console.log('writeuser', err);
+    });
     console.log('Expected result', result);
     return result;
   }
